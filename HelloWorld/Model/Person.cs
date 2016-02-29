80bd07a15ce158992bbb977f5e5476bf6085e212
AlexanderRN <AlexanderRNielsen@hotmail.dk>
2016-02-29 15:24:08 +0100

    * Person.cs: * HelloWorld.csproj: Added a Person class.
    * Program.cs: Added some usages for Person class.

---
+++ HelloWorld/Model/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Model
+{
+	public class Person
+	{
+		public int Id { get; private set; }
+
+		public string FirstName { get; set;}
+		private string lastName;
+		private long value;
+
+		public string LastName {
+			get { return lastName; }
+			set {
+				if (value == null)
+					value = "Unknown";
+				if(value.Length < 2) 
+					throw new ArgumentOutOfRangeException();
+				lastName = value; 
+			}
+		}
+
+		public decimal Value {
+			get { return value/100m; }
+			set { this.value = (long)(100*value); }
+		}
+
+		public Person(int id, string firstName, string lastName) {
+			Id = id;
+			FirstName = firstName;
+			//this.lastName = lastName; Bruger ikke LastName Check.
+			LastName = lastName;
+		}
+
+		public string Name {
+			get { return FirstName + " " + LastName; }
+			set { 
+				string[] parts = value.Split (" ");
+				if (parts.Length < 2)
+					return;
+				FirstName = parts[0];
+				LastName = parts [1];
+			}
+		}
+	}
+}
+
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Model;
 
 namespace HelloWorld
 {
@@ -8,11 +9,25 @@ namespace HelloWorld
 		{
 			Console.WriteLine ("Hello World!");
 
+			Person p = new Person (7, "Test", "Testesen");
+			Console.WriteLine ("ID: " + p.Id);
+			Console.WriteLine ("Firstname: " + p.FirstName);
+			Console.WriteLine ("Lastname: " + p.LastName);
 
-			// Dette er en Git Test
-			Console.WriteLine ("Test Update!");
+			// p.Id = 8; Brokker sig over privat id
+			 p.LastName = null;
+			 Console.WriteLine ("Lastname: " + p.LastName);
+			try {
+				p.LastName = "F";
+				Console.WriteLine ("Lastname: " + p.LastName);
+			} catch (ArgumentOutOfRangeException){
+				Console.WriteLine ("The Lastname you submitted was out of the accepted range, please try again.");
+			} catch {
+				Console.WriteLine ("Ukendt fejl, kontakt support");
+			}
+		
 
-			//Test2
+			//Person p1 = new Person (6, "Test", "T");
 		}
 	}
 }

3c84d90e74eb0dd0bb668347ff0186d5228faddd
AlexanderRN <AlexanderRNielsen@hotmail.dk>
2016-02-29 13:27:40 +0100

    Added a comment and a new console line.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,10 @@ namespace HelloWorld
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+
+
+			// Dette er en Git Test
+			Console.WriteLine ("Test Update!");
 		}
 	}
 }
